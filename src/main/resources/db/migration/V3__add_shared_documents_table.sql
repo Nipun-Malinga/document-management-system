8cedf554350dc6a7b0df328afdcbfc611d46473f
Nipun Malinga <nipunmalinga11@gmail.com>
2025-08-08 17:16:05 +0530

    refac: add cascading to sql tables

--- src/main/resources/db/migration/V3__add_shared_documents_table.sql
+++ src/main/resources/db/migration/V3__add_shared_documents_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE shared_documents (
     user_id BIGINT NOT NULL,
     permission VARCHAR(20) DEFAULT 'READ_ONLY',
     CONSTRAINT fk_shared_document_id
-        FOREIGN KEY(document_id) REFERENCES documents(id),
+        FOREIGN KEY(document_id) REFERENCES documents(id) ON DELETE CASCADE,
     CONSTRAINT fk_shared_user_id
-        FOREIGN KEY(user_id) REFERENCES users(id)
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 )
--- src/main/resources/db/migration/V4__add_document_versions_table.sql
+++ src/main/resources/db/migration/V4__add_document_versions_table.sql
@@ -3,7 +3,6 @@ CREATE TABLE document_version_content (
   content LONGTEXT
 );
 
-
 CREATE TABLE document_version (
     id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     document_id BIGINT NOT NULL,
@@ -12,9 +11,9 @@ CREATE TABLE document_version (
     author BIGINT NOT NULL,
     timestamp DATETIME,
     CONSTRAINT fk_version_document_id
-        FOREIGN KEY (document_id) REFERENCES documents(id),
+        FOREIGN KEY (document_id) REFERENCES documents(id) ON DELETE CASCADE ,
     CONSTRAINT fk_version_content_id
         FOREIGN KEY (version_content_id) REFERENCES document_version_content(id),
     CONSTRAINT fk_version_user_id
-        FOREIGN KEY (author) REFERENCES users(id)
+        FOREIGN KEY (author) REFERENCES users(id) ON DELETE CASCADE
 )
